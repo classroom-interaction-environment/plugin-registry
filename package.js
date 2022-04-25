2b0bc219a979f329894bc700d6fe9d94603c3de5
Jan Küster <jkuester@uni-bremen.de>
2022-04-25 11:45:56 +0200

    core implemented

--- package.js
+++ package.js
@@ -2,17 +2,12 @@
 Package.describe({
   name: 'claire:plugin-registry',
   version: '1.0.0',
-  // Brief, one-line summary of the package.
-  summary: '',
-  // URL to the Git repository containing the source code for this package.
+  summary: 'Base package to register plugins for CLAIRE',
   git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
 })
 
 Package.onUse(function (api) {
-  api.versionsFrom('1.6')
   api.use('ecmascript')
   api.mainModule('plugin-registry.js')
 })
